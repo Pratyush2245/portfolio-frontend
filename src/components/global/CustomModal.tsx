354b901272cee0f13ea956c51749553fdf0bd16c
PratyushBhattacharya123 <pratyushbhattacharya33@gmail.com>
2024-04-07 00:52:39 +0530

    initial commit

--- src/components/global/CustomModal.tsx
+++ src/components/global/CustomModal.tsx
@@ -24,6 +24,7 @@ const CustomModal: React.FC<Props> = ({
       onClose={() => setOpen(false)}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
+      className="z-[100000000]"
     >
       <Box className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 md:w-[300px] w-[75vw] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
         <Component setOpen={setOpen} setRoute={setRoute} refetch={refetch} />
